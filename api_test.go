ca57421076bf1143c6a0f46a90617cd6da5345e2
David Luevano Alvarado <david@luevano.xyz>
2024-05-19 02:57:31 -0600

    feat: add very minimal testing

---
+++ api_test.go
@@ -0,0 +1,36 @@
+package mangoplus
+
+import "testing"
+
+var client = NewPlusClient()
+
+//
+// manga.go
+//
+
+func TestMangaGet(t *testing.T) {
+	// Ghost fixers
+	_, err := client.Manga.Get("100310")
+	if err != nil {
+		t.Errorf("Getting manga by id: %s\n", err.Error())
+	}
+}
+
+func TestMangaAll(t *testing.T) {
+	_, err := client.Manga.All()
+	if err != nil {
+		t.Errorf("Getting all manga: %s\n", err.Error())
+	}
+}
+
+//
+// page.go
+//
+
+func TestPageGet(t *testing.T) {
+	// Ghost fixers' first chapter
+	_, err := client.Page.Get("1020497", false, ImageQualityHigh)
+	if err != nil {
+		t.Errorf("Getting chapter pages by id: %s\n", err.Error())
+	}
+}
